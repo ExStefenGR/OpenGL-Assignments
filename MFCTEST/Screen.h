c482c9e6c569a42408771f54191a3a2a7771380b
Stefanos Triantafyllidis <stt0242@my.londonmet.ac.uk>
2021-10-26 18:01:26 +0100

    Add dependencies/extensions

---
+++ MFCTEST/Screen.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <Windows.h>
+#include <glm.hpp>
+#include <string>
+#include "wglext.h"
+#include "glad.h"
+
+class Screen
+{
+
+public:
+
+	static Screen* Instance();
+
+	void SetViewport(GLint x, GLint y, GLsizei width, GLsizei height);
+	bool Initialize(HWND wnd, const std::string& filename);
+
+	void ActivateContext();
+	void DeactivateContext();
+
+	void Refresh();
+	void Present();
+	void Shutdown();
+
+private:
+
+	Screen();
+	Screen(const Screen&);
+	Screen& operator=(const Screen&);
+
+	GLint m_pixelFormat;
+
+	glm::ivec4 m_viewport;
+	glm::uvec2 m_resolution;
+
+	HDC m_deviceContext;
+	HGLRC m_renderContext;
+
+	PIXELFORMATDESCRIPTOR m_pixelFormatClass;
+	PFNWGLCREATECONTEXTATTRIBSARBPROC m_wglCreateContextAttribsARB;
+
+};
